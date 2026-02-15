e871519db646f17795ad3114eb7863b694a6f6a1
Ahmed Abdelnaser <ahmeducfjf@gmail.com>
2026-02-15 06:46:51 +0200

    Fix:Change the return value from IEnumerable to IReadOnlyList

--- SmartMentor.Abstraction/Repositories/IGenericRepository.cs
+++ SmartMentor.Abstraction/Repositories/IGenericRepository.cs
@@ -4,12 +4,12 @@ namespace SmartMentor.Abstraction.Repositories
 {
     public interface IGenericRepository<T>  where T : class
     {
-        Task<IEnumerable<T>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<IReadOnlyList<T>> GetAllAsync(CancellationToken cancellationToken = default);
         Task<T?> GetByIdAsync(object[]keyvalues, CancellationToken cancellationToken = default);
         Task AddAsync(T entity, CancellationToken cancellationToken = default);
         void Update(T entity);
         void Delete(T entity);
-        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default);
+        Task<IReadOnlyList<T>> FindAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default);
         Task<bool> AnyAsync(Expression<Func<T, bool>> predicate, CancellationToken cancellationToken = default);
         Task<int?> CountAsync(Expression<Func<T, bool>>? predicate = null, CancellationToken cancellationToken = default);
 
