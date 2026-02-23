66fe4e40a820fbf05d21cd1e23df75fb9b774ff0
Ahmed Abdelnaser <ahmeducfjf@gmail.com>
2026-02-23 04:12:39 +0200

    Feat:Add DataNotations for some validation

--- SmartMentor.Abstraction/Dto/Requests/UserRequests/CompleteUserProfileRequest.cs
+++ SmartMentor.Abstraction/Dto/Requests/UserRequests/CompleteUserProfileRequest.cs
@@ -1,22 +1,21 @@
 ﻿using SmartMentor.Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace SmartMentor.Abstraction.Dto.Requests.UserRequests
 {
     public class CompleteUserProfileRequest
     {
         public List<UserSkillRequest> Skills { get; set; } = new List<UserSkillRequest>();
+        [Required(ErrorMessage = "Interests are required")]
         public List<int> InterestIds { get; set; } = new List<int>();
+        [Required(ErrorMessage = "Career goal is required")]
         public int CareerGoalId { get; set; }
     }
 
     public class UserSkillRequest
     {
+        [Required(ErrorMessage = "SkillId is required")]
         public int SkillId { get; set; }
+        [Required(ErrorMessage = "SkillLevel is required")]
         public SkillLevelEnum SkillLevel { get; set; }
     }
 }
